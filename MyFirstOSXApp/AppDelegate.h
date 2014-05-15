36dc4c3ee277634ab0b25c6ba172fce247878504
Logan Wright <logan.william.wright@gmail.com>
2014-05-15 01:51:22 -0400

    Initial Commit

---
+++ MyFirstOSXApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyFirstOSXApp
+//
+//  Created by Logan Wright on 5/15/14.
+//  Copyright (c) 2014 Logan Wright. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MyFirstOSXApp/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MyFirstOSXApp
+//
+//  Created by Logan Wright on 5/15/14.
+//  Copyright (c) 2014 Logan Wright. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
